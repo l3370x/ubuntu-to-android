7d8cf413958da60319960bb47234a152de493b62
Aaron Nelson Miller <aaronm3@microbe.cs.byu.edu>
2013-02-07 10:35:49 -0700

    added a for looper

--- helloOUT.c
+++ helloOUT.c
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	cout << "hello" << endl;
+	cout << "hello is OUT" << endl;
+	for( int i = 0; i < 10 ; ++i){
+		cout << "\t" << i << endl;
+	}
 	return 0;
 }
